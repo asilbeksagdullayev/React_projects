436d8e375eb4e9027b8387b42bb2d92b36831303
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2023-12-20 22:23:59 +0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+export default class Main extends Component {
+	className = "flex";
+	render() {
+		return <div className="container bg-ba font-ionic flex">Main</div>;
+	}
+}
